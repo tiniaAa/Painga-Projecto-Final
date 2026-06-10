046c73f51437400b58d01871e2be0f679f89029b
tiniaAa <franciscovolovik@gmail.com>
2026-06-10 10:47:01 -0300

    comienzo de manipulacion del DOM

---
+++ js/javascript.js
@@ -0,0 +1,46 @@
+
+//Declaracion variables
+let Catalogo=[];
+const URLcatalogo ="../db/data.json";
+
+
+const rowContainer = document.querySelector(".row")
+//
+
+function ObtenerDatos(){
+    fetch(URLcatalogo)
+         .then(response => response.json())
+         .then(data => {
+            Catalogo=data;
+            renderProductos(Catalogo)
+         });
+}
+
+
+function renderProductos(listaProductos) {
+    if (!rowContainer) return;
+    rowContainer.innerHTML = "";
+
+    listaProductos.forEach(producto => {
+        const columna = document.createElement("div");
+        columna.className = "col-md-3"; 
+
+        // 2. El innerHTML arranca directo desde la card (sin envolverlo en otra columna)
+        columna.innerHTML = `
+        <div class="card h-100">
+            <img src="${producto.imagen}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p>${producto.nombre}</p>
+                <p class="card-text">${producto.descripcion}</p>
+                <p>Precio: $${producto.precio}</p>
+                <div>
+                    <button class="agregarCarrito" data-id="${producto.id}">Agrega al carrito</button>
+                </div>
+            </div>
+        </div>
+    `
+        rowContainer.appendChild(columna);
+    });
+}
+
+ObtenerDatos();
